eaa1a15e6d90f9ce1ea354e6b752bcbb8ce631ff
Arman Anzanpour <armanz@utu.fi>
2017-03-16 18:30:56 +0200

    The first version of code created for demonstration 5 of IoT course, tested with Python 3.5 on windows.

---
+++ read-send.py
@@ -0,0 +1,28 @@
+import serial
+#import urllib2
+#urllib2.urlopen("http://example.com/foo/bar").read()
+
+import urllib.request
+
+
+
+portnumber = input('Enter port number: ')
+ser = serial.Serial('COM'+portnumber, 9600, timeout=0)
+ 
+print("connected to: " + ser.portstr)
+theline=""
+while True:
+    for line in ser.read():
+            if line!=13 and line!=10:
+                line = chr(line)
+                theline=theline+line
+            else:
+                print(theline.rstrip())
+                urllib.request.urlopen("http://arman.in/iot/sendme.php?userid=922&value="+theline.rstrip()).read()
+                theline=""
+
+
+
+ser.close()
+
+
